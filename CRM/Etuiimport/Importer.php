e17f4b536a56b3231f3c7f70eb06604dfdb38d3c
Alain Benbassat <alain@businessandcode.eu>
2019-02-11 17:41:22 +0100

    import hesamag

--- CRM/Etuiimport/Importer.php
+++ CRM/Etuiimport/Importer.php
@@ -53,11 +53,14 @@ class CRM_Etuiimport_Importer {
     // select the subscriber
     $sql = "
       SELECT
-        *
+        h.*
+        , ctry.id country_id
       FROM
-        tmpetui_hesamag
+        tmpetui_hesamag h
+      left outer join
+        civicrm_country ctry on h.country = ctry.name
       WHERE
-        id = $id
+        h.id = $id
     ";
     $dao = CRM_Core_DAO::executeQuery($sql);
     if ($dao->fetch()) {
@@ -67,6 +70,17 @@ class CRM_Etuiimport_Importer {
       // find contact by name and organization
       $contactID = self::findContactByName($dao->organization, $dao->firstName, $dao->lastName, $status);
 
+      if ($contactID == 0) {
+        // the contact was not found, we create it
+        $contactID = self::createHesaMagContact($dao->organization, $dao->prefix, $dao->firstName, $dao->lastName, $status);
+      }
+      
+      if ($contactID > 0) {
+        // add the magazine address and subscription
+        self::createHesaMagAddress($contactID, $dao, $status);  
+      }
+      
+      // update the status
       $updateSQL = "update tmpetui_hesamag set status = %1 where id = %2";
       $updateParams = [
         1 => [$status, 'String'],
@@ -76,6 +90,185 @@ class CRM_Etuiimport_Importer {
     }
 
     return TRUE;
+  }
+
+  public static function createHesaMagContact($organization, $prefix, $firstName, $lastName, &$status) {
+    if ($firstName == '-' || $firstName == '--') {
+      $firstName = '';
+    }
+    if ($lastName == '-' || $lastName == '--') {
+      $lastName = '';
+    }
+
+    if ($firstName && $lastName) {
+      $params = [
+        'first_name' => $firstName,
+        'last_name' => $lastName,
+        'contact_type' => 'Individual',
+        'sequential' => 1,
+        'source' => 'HESAMAG import',
+      ];
+    }
+    elseif ($organization && strpos($organization, $lastName) == FALSE) {
+      $params = [
+        'organization_name' => $lastName,
+        'contact_type' => 'Organization',
+        'sequential' => 1,
+        'source' => 'HESAMAG import',
+      ];
+    }
+    else {
+      $status = 'Cannot create contact';
+      return -1;
+    }
+
+    $result = civicrm_api3('Contact', 'create', $params);
+    $status = 'created';
+    return $result['id'];
+  }
+
+  public static function createHesaMagAddress($contactID, $dao, &$status) {
+    // get some info about this contact
+    $sql = "
+      select
+        c.contact_type
+        , a.id address_id
+        , m_en.id mag_en_id
+        , m_fr.id mag_fr_id 
+      from
+        civicrm_contact c
+      left outer join
+        civicrm_address a on a.contact_id = c.id and a.location_type_id = 8
+      left outer join
+        civicrm_membership m_en on m_en.contact_id = m_en.id and m_en.membership_type_id = 1 
+      left outer join
+        civicrm_membership m_fr on m_fr.contact_id = m_fr.id and m_fr.membership_type_id = 2 
+      where
+        c.id = $contactID 
+    ";
+    $contact = CRM_Core_DAO::executeQuery($sql);
+    if (!$contact->fetch()) {
+      $status = "Cannot find contact with ID = $contactID";
+      return;
+    }
+
+    /*
+
+     */
+    // create the address
+    $addressParams = [
+      'contact_id' => $contactID,
+      'location_type_id' => 8,
+      'street_address' => $dao->street_address,
+      'city' => $dao->city,
+      'postal_code' => $dao->postal_code,
+    ];
+
+    if ($dao->country_id) {
+      $addressParams['country_id'] = $dao->country_id;
+    }
+    elseif ($dao->country) {
+      // this is a country that was not found in the country table, manually correct is
+      $addressParams['country_id'] = self::getCountryID($dao->country);
+    }
+    else {
+      // make belgium the default country
+      $addressParams['country_id'] = 1020;
+    }
+
+    if ($contact->address_id) {
+      // update existing address
+      $addressParams['id'] = $contact->address_id;
+    }
+
+    // FORMAT THE ADDRESS
+
+    // check for a person name
+    if ($dao->first_name || $dao->last_Name) {
+      $name = $dao->first_name . ' ' . $dao->last_Name;
+    }
+      // check if it's a real organization (ie. not a fake from Synergy)
+    if ($dao->organization && strpos($dao->organization, $dao->last_Name) == FALSE) {
+      $addressParams['supplemental_address_1'] = $dao->organization;
+
+      if ($name && $dao->department) {
+        $addressParams['supplemental_address_2'] = $dao->department;
+        $addressParams['supplemental_address_3'] = 'Attn. ' . $name;
+      }
+      elseif ($dao->department) {
+        $addressParams['supplemental_address_2'] = $dao->department;
+      }
+      elseif ($name) {
+        $addressParams['supplemental_address_2'] = 'Attn. ' . $name;
+      }
+    }
+    else {
+        // person
+      if ($name && $dao->department) {
+        $addressParams['supplemental_address_1'] = $dao->department;
+        $addressParams['supplemental_address_2'] = 'Attn. ' . $name;
+      }
+      elseif ($dao->department) {
+        $addressParams['supplemental_address_1'] = $dao->department;
+      }
+      elseif ($name) {
+        $addressParams['supplemental_address_1'] = $name;
+      }
+    }
+
+    civicrm_api3('Address', 'create', $addressParams);
+
+    // create the subscription
+    $magParams = [
+      'join_date' => '2018-04-01',
+      'start_date' => '2018-04-01',
+      'contact_id' => $contactID,
+      'source' => 'HesaMag Import',
+      'status_id' => 2,
+    ];
+
+    if ($dao->magazine_lang == 'EN') {
+      $magParams['membership_type_id'] = 1;
+
+      // udpate or create?
+      if ($contact->mag_en_id) {
+        $magParams['id'] = $contact->mag_en_id;
+      }
+    }
+    elseif ($dao->magazine_lang == 'FR') {
+      $magParams['membership_type_id'] = 2;
+
+      // udpate or create?
+      if ($contact->mag_fr_id) {
+        $magParams['id'] = $contact->mag_fr_id;
+      }
+    }
+    else {
+      $status = 'Invalid magazine language';
+      return;
+    }
+
+  }
+
+  public static function getCountryID($country) {
+    if ($country == 'United States of America') {
+      return 1228;
+    }
+    elseif ($country == 'Azerbaidjan') {
+      return 1015;
+    }
+    elseif ($country == 'Macedonia') {
+      return 1128;
+    }
+    elseif ($country == 'South-Korea') {
+      return 1115;
+    }
+    elseif ($country == 'The Democratic Republic of the Congo') {
+      return 1050;
+    }
+    else {
+      return 1020; // belgium
+    }
   }
 
   public static function findContactByName($organization, $firstName, $lastName, &$status) {
@@ -105,7 +298,7 @@ class CRM_Etuiimport_Importer {
       elseif ($result['count'] > 0) {
         // multiple matches
         $status = "multiple matches for organissation + first name + last name";
-        return 0;
+        return -1;
       }
     }
 
@@ -127,7 +320,7 @@ class CRM_Etuiimport_Importer {
       elseif ($result['count'] > 0) {
         // multiple matches
         $status = "multiple matches for first name + last name";
-        return 0;
+        return -1;
       }
     }
 
@@ -149,7 +342,7 @@ class CRM_Etuiimport_Importer {
       elseif ($result['count'] > 0) {
         // multiple matches
         $status = "multiple matches for last name + organization";
-        return 0;
+        return -1;
       }
     }
 
@@ -170,7 +363,7 @@ class CRM_Etuiimport_Importer {
       elseif ($result['count'] > 0) {
         // multiple matches
         $status = "multiple matches for organization";
-        return 0;
+        return -1;
       }
     }
 
